c25c6b332fa9459ca44d7064a67e64f4ee8aa36e
nathanrwatt-web <nathanrwatt@gmail.com>
2026-04-08 15:21:56 -0400

    changed search call for cargo check

--- src/main.rs
+++ src/main.rs
@@ -85,11 +85,11 @@ fn run (config: Config) -> Result<(), Box<dyn Error>> {
     let results = if config.ignore_case {
         search_case_insensitive(&config.query, &contents)
     } else {
-        search(&config.query, &contents)
+        search_regex(&config.query, &contents)
     };
 
-    for line in results {
-        println!("{line}");
+    for (int, line) in results {
+        println!("{int}   {line}");
     }
     
     Ok(())
